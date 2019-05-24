c0a261e7546d78874e001ef71246f3185fc894ff
Evgeniy Morozov <evgeniy.morozov@dsr-corporation.com>
2019-05-24 23:14:23 +0300

    Initial project setup

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Evgeniy Morozov on 2019-05-24.
+//
+#include <iostream>
+
+int main(int argc, char * const argv[])
+{
+    std::cout << "Hello, world" << std::endl;
+    return 0;
+}
