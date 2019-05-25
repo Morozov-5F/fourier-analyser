5355a423d34ab39411acfa51b4a87e119f5b3503
E Morozov <zhe3145@yandex.ru>
2019-05-25 12:48:09 +0300

    Added wildcard file processing

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,12 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <regex>
+#include <set>
+#include <fstream>
 
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 
 using namespace boost::program_options;
 
@@ -15,6 +19,7 @@ int main(int argc, const char * const argv[])
 {
     std::int64_t start, end;
     std::string out_dir;
+    std::vector<std::string> filenames;
 
     try {
         options_description available_options("Available Options");
@@ -26,7 +31,7 @@ int main(int argc, const char * const argv[])
 
         options_description hidden_opts;
         hidden_opts.add_options()
-                ("file_list", value<std::vector<std::string>>()->multitoken()->zero_tokens()->composing(), "Files to process");
+                ("file_list", value<std::vector<std::string>>(&filenames)->multitoken(), "Files to process");
 
         options_description all_opts;
         all_opts.add(available_options);
@@ -45,15 +50,58 @@ int main(int argc, const char * const argv[])
 
         if (vm.count("help")) {
             std::cout << available_options << std::endl;
-        } else if (vm.count("file_list")) {
-            std::copy(vm["file_list"].as<std::vector<std::string>>().begin(),
-                      vm["file_list"].as<std::vector<std::string>>().end(),
-                      std::ostream_iterator<std::string>{std::cout, "\n"});
+        } else if (!vm.count("file_list")) {
+            throw error("Nothing to do - no file specified");
         }
     }
-    catch (const error &ex)
-    {
+    catch (const error &ex) {
         std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
     }
+
+    std::set<boost::filesystem::path> filename_set;
+
+    for (auto const & filename : filenames) {
+        boost::filesystem::path target_path(filename);
+        auto target_path_name = target_path.parent_path().string();
+        if (target_path_name.empty()) {
+            target_path_name = "./";
+        }
+
+        // Convert glob pattern to regex escaping all possible control sequences
+        auto filename_wildcarded = target_path.filename().string();
+        filename_wildcarded = std::regex_replace(filename_wildcarded, std::regex(R"((\~|\^|\{|\}|\s|\(|\)|\/|\\))"), "\\$1");
+        filename_wildcarded = std::regex_replace(filename_wildcarded, std::regex("\\."), "\\.");
+        filename_wildcarded = std::regex_replace(filename_wildcarded, std::regex("\\*"), ".*");
+        filename_wildcarded = std::regex_replace(filename_wildcarded, std::regex("\\?"), ".");
+
+        const std::regex my_filter(filename_wildcarded);
+
+        boost::filesystem::directory_iterator end_itr; // Default ctor yields past-the-end
+        for( boost::filesystem::directory_iterator i(target_path_name); i != end_itr; ++i ) {
+            if(!boost::filesystem::is_regular_file(i->status())) {
+                continue;
+            }
+
+            auto current_filename = i->path().filename().string();
+            if (!std::regex_match(current_filename, my_filter)) {
+                continue;
+            }
+
+            filename_set.insert(i->path());
+        }
+    }
+
+    for (auto const& path : filename_set) {
+        std::vector<double> data;
+        std::ifstream f(path.string());
+        std::istream_iterator<double> input(f);
+        std::copy(input, std::istream_iterator<double>(), std::back_inserter(data));
+        if (data.empty()) {
+            std::cout << "File " << path << " does not contain any data, skipping" << std::endl;
+        }
+//        std::copy(data.begin(), data.end(), std::ostream_iterator<double>(std::cout, "\n"));
+    }
+
     return 0;
 }
